145d77b09a3777e4369d2651313218f00a79ac88
Nightfall <marcelantonio246@gmail.com>
2023-09-21 16:11:33 +0700

    testing deploy again

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './home.css'
 import axios from 'axios';
-import Card from '../../components/card/card';
 import Navbar from '../navbar/navbar';
+import Card from '../../components/card/card';
 
 const Home = () => {
     const [products, setProducts] = useState([]);
