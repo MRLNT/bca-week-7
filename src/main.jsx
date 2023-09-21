a5c813bff21803cf21847a4378cf5805a924185b
Nightfall <marcelantonio246@gmail.com>
2023-09-21 16:20:58 +0700

    test api

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './pages/home';
-// import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Router from './router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
     <Router />
   </React.StrictMode>,
 );
